989ef4c7b6fd5a7b22df2aba9296802f7a901409
souichi1204 <54573259+souichi1204@users.noreply.github.com>
2020-03-10 20:30:02 +0900

    Add files via upload

---
+++ 3章.py
@@ -0,0 +1,29 @@
+#コメント
+x=10
+print(x)
+print(x/2)
+print(x*2)
+#比較演算子を使うとtrue or false
+print(x<100)
+#if文
+if(x==100):
+    print("x=100")
+elif x==10:
+        print("xは10です")
+else:
+    print("xは100ではない")
+
+n=10
+
+#2行以上からなる文を複合文。forのコロンがある文がヘッダー、その下のprintがスイート
+
+for n in range(10):
+    print("hello,world")
+
+#以下チャレンジ
+print("5")
+print("ww")
+if x<10:
+    print("xは10未満")
+else:
+    print("ｘは10以上")
